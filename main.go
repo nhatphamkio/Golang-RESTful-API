a9165c8c4f7b9fc64eb01e8ab4d8aa99bbc6e28b
nhatpham <nhatphamkio@gmail.com>
2020-07-15 08:58:15 +0900

    Creating a Simple RESTful API

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
-type 
+type Article struct{
+	Title string `json:"Title"`
+	Desc string `json:"dess"`
+	Content string `json:"content"`
+}
+type Articles []Article
+func allArticles(w http.ResponseWriter,r *http.Request){
+	articles := Articles{
+		Article{Title:"Test Tittle",Desc:"Test Description",Content:"Hello"},
+	}
+	fmt.Println("Endpoint Hit:All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 func homePage(w http.ResponseWriter,r *http.Request){
 	fmt.Fprintf(w,"Homepage Endpoint Hit")
 }
 func handleRequest(){
 	http.HandleFunc("/",homePage)
+	http.HandleFunc("/articles",allArticles)
 	log.Fatal(http.ListenAndServe(":8001",nil))
 
 }
